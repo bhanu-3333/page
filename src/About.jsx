39f5e732ccfc654118b7f51a31e7753cc84ca4e3
b75488460 <b75488460@gmail.com>
2025-03-05 17:19:05 +0000

    about

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <h2>About This App</h2>
+        <p>This is a simple React app with a counter and navigation.</p>
+      </div>
+    );
+  }
+  
+  export default About;
+  
